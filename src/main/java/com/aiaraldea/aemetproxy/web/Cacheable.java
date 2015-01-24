b64cdb80fc78063feb171199d1200cf5c130838c
Iñaki Ibarrola Atxa <inakiibarrola@yahoo.es>
2015-01-24 01:38:07 +0100

    Add cache headers

---
+++ src/main/java/com/aiaraldea/aemetproxy/web/Cacheable.java
@@ -0,0 +1,12 @@
+package com.aiaraldea.aemetproxy.web;
+
+import java.lang.annotation.*;
+import javax.ws.rs.NameBinding;
+
+@NameBinding
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Cacheable {
+
+    String cc() default "public, must-revalidate";
+}
